9465fe974564bd609f07ebc8e1b8f92aab954b03
JuhaszAlex <juhaszalex95@gmail.com>
2018-09-21 00:31:23 +0200

    zsa

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://secure-harbor-71820.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
